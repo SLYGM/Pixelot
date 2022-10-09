089ef22a69b6d6d8fa02f4bf91caa4779c771450
DaedraKyne <sebmodley@gmail.com>
2022-10-09 13:11:14 +0100

    Add bouncy behaviour to black hole

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import {createSprite, addToUpdateQueue, begin_rendering} from './renderer/renderer.js';
 
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const gl = canvas.getContext("webgl2") as WebGL2RenderingContext;
 let p = {
     sprite: createSprite(0, 0, './images/BlackHole.png'),
+    dx: 100,
+    dy: 100,
     update: function(dt: number){
-        this.sprite.x += 10 * dt;
-        this.sprite.y += 10 * dt
+        this.sprite.x += this.dx * dt;
+        this.sprite.y += this.dy * dt
+        if (this.sprite.x + this.sprite.tex.width > gl.canvas.clientWidth || this.sprite.x < 0){
+            this.dx *= -1;
+        }
+        if (this.sprite.y + this.sprite.tex.height > gl.canvas.clientHeight || this.sprite.y < 0){
+            this.dy *= -1;
+        }
     }
 }
 addToUpdateQueue(p);
