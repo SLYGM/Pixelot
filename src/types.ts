f7e034fbc2d883fa34b659cd3e73f661e83de44a
Maxim Gorbach <Maxim.Gorbach@warwick.ac.uk>
2022-11-12 12:26:31 +0000

    Removed old sprite type

--- src/types.ts
+++ src/types.ts
@@ -4,11 +4,6 @@ type Texture = {
     texture: WebGLTexture | null;
 }
 
-type Sprite = {
-    x: number;
-    y: number;
-    tex: Texture;
-}
 
 type Updatable = {
     update: (dt: number) => void;
