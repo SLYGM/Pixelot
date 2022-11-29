c4b674634e71fa9ec17f0e00c9773a9aa547ddad
DaedraKyne <sebmodley@gmail.com>
2022-11-29 22:24:48 +0000

    Animated space invader collider sprite

--- engine/src/main.ts
+++ engine/src/main.ts
@@ -4,7 +4,7 @@ import { PostProcessing } from './renderer/post_process.js';
 import { BarShader } from './renderer/post effects/bars.js';
 import BarrelShader from './renderer/post effects/barrel.js';
 
-import { GameObjectBase } from "./ecs.js";
+import { GameObjectBase, System } from "./ecs.js";
 import { $scene } from "./sceneManager.js";
 import { Game } from "./gameloop.js";
 
@@ -13,6 +13,7 @@ import "./scriptManager.js";
 
 import Position from "./components/Position.js";
 import Sprite from "./components/Sprite.js";
+import SpriteCollider from "./components/SpriteCollider.js";
 
 import { KeyStates } from "./keyState.js";
 import { MouseState } from "./mouseState.js";
@@ -20,35 +21,61 @@ import { MouseState } from "./mouseState.js";
 import './scriptManager.js';
 import './componentManager.js';
 import CRTShader from "./renderer/post effects/crt.js";
+// import AnimatedSprite from "./components/AnimatedSprite.js";
+import { ImportManager } from "./componentManager.js";
+import SpriteAnimationSystem from "./systems/SpriteAnimationSystem.js";
+import AnimatedSprite from "./components/AnimatedSprite.js";
+// import SpriteAnimationSystem from "./systems/SpriteAnimationSystem.js";
 
-Renderer.setResolution(426, 240);
-Renderer.addLayer(new SpriteLayer(), 'sprites');
+Renderer.setResolution(256, 128);
+const l = new SpriteLayer();
+Renderer.addLayer(l, 'sprites');
 Renderer.loadTexture('./images/frog.png', 'frog')
 Renderer.loadTexture('./images/tile.png', 'tile')
+for (let i of [0,1]) 
+    Renderer.loadTexture(`./images/bottom_invader ${i}.png`, `invader_${i}`)
 
-class TestEntity extends GameObjectBase {
+class InvaderEntity extends GameObjectBase {
     onCreate(): void {
-        this.get(Position).x = 50;
-        this.get(Position).y = 50;
     }
 
     update(): void {
-        if (KeyStates.isPressed("a")) {
-            this.get(Position).x = 50;
-            this.get(Position).y = 50;
-        } else {
-            this.get(Position).x = MouseState.world_pos.x;
-            this.get(Position).y = MouseState.world_pos.y;
+        if (this.get(SpriteCollider).isColliding({
+            x: MouseState.world_pos.x,
+            y: MouseState.world_pos.y
+        },
+        {
+            width: 1, height: 1
+        })) {
+            console.log("pam!");
+            $scene.deleteEntity(this);
+            $scene.update();
         }
+        // if (KeyStates.isPressed("a")) {
+        //     this.get(Position).x = 50;
+        //     this.get(Position).y = 50;
+        // } else {
+        //     this.get(Position).x = MouseState.world_pos.x;
+        //     this.get(Position).y = MouseState.world_pos.y;
+        // }
     }
+
 }
 
-const t = new TestEntity("test");
-t.add(new Position()).add(new Sprite("frog", "sprites", 1));
-$scene.addEntity(t);
+$scene.addSystem(new SpriteAnimationSystem(), 1);
 
-PostProcessing.add(new BarrelShader());
-PostProcessing.add(new CRTShader());
+//Setup scene
+for (let i = 0; i < 256/16 - 4; i++) {
+    let invader = new InvaderEntity("invader"+i);
+    invader.add(new Position).add(new AnimatedSprite("invader", "sprites", 7.5, 2, 1)).add(new SpriteCollider());
+    let pos = invader.get(Position);
+    pos.x = 16 * i + 32; pos.y = 50;
+    $scene.addEntity(invader);
+}
+
+
+// PostProcessing.add(new BarrelShader());
+// PostProcessing.add(new CRTShader());
 
 Game.addToUpdateQueue($scene);
 Game.start();
