fc8cfd7626384150645535204aeaba79da5fe20a
Louis Tarvin <me@louistarvin.uk>
2022-11-29 12:45:38 +0000

    Fix sidebar not updating on scene change

--- sugma/src/app/scene-tab/scene-tab.component.ts
+++ sugma/src/app/scene-tab/scene-tab.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { SceneManagerService } from 'app/scene-manager.service';
 import { Entity, Scene } from 'types';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'scene-tab',
@@ -13,18 +14,33 @@ export class SceneTabComponent {
   scenes?: Map<string, Scene>;
   scene?: Scene;
   selectedEntity?: Entity;
+  private sub;
 
-  constructor(private route: ActivatedRoute, private sceneManager: SceneManagerService) { }
-
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.sceneName = params['sceneName'];
+  constructor(private router: Router, 
+              private route: ActivatedRoute, 
+              private sceneManager: SceneManagerService) 
+  {
+    this.sub = router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.route.params.subscribe(params => {
+        this.sceneName = params['sceneName'];
+      });
+      if (this.sceneName) {
+        this.scene = this.sceneManager.getScene(this.sceneName);
+      } 
+      if (!this.scene) {
+        // The scene doesn't exist, so redirect home
+        console.log("Scene doesn't exist, redirecting home");
+        this.router.navigate(['/']);
+        return;
+      } else if (this.scene.entities.length > 0) {
+        this.selectedEntity = this.scene.entities[0];
+      }
     });
-    if (this.sceneName) {
-      this.scene = this.sceneManager.getScene(this.sceneName);
-    }
-    if (this.scene && this.scene.entities.length > 0) {
-      this.selectedEntity = this.scene.entities[0];
-    }
   }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+}
 }
