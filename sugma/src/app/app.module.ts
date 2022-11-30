0ad6e683ab0bc4e98375c23f59e4f895a9c496a6
Louis-Tarvin <louis.tarvin@gmail.com>
2022-11-30 16:23:28 +0000

    Add component dialog

--- sugma/src/app/app.module.ts
+++ sugma/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
-import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+import { AddComponentDialog, RightSidebarComponent } from './right-sidebar/right-sidebar.component';
 import { FileManagerComponent } from './file-manager/file-manager.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SceneTabComponent } from './scene-tab/scene-tab.component';
@@ -14,6 +14,10 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -21,6 +25,7 @@ import { MatInputModule } from '@angular/material/input';
     ToolbarComponent,
     LeftSidebarComponent,
     RightSidebarComponent,
+    AddComponentDialog,
     FileManagerComponent,
     NavbarComponent,
     SceneTabComponent
@@ -29,10 +34,14 @@ import { MatInputModule } from '@angular/material/input';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatTabsModule,
     MatExpansionModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatDialogModule,
+    MatAutocompleteModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- sugma/src/app/right-sidebar/right-sidebar.component.ts
+++ sugma/src/app/right-sidebar/right-sidebar.component.ts
@@ -1,4 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 import { Entity } from 'types';
 
 @Component({
@@ -8,4 +13,61 @@ import { Entity } from 'types';
 })
 export class RightSidebarComponent {
   @Input() entity?: Entity;
+
+  constructor(public dialog: MatDialog, private _snackBar: MatSnackBar) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddComponentDialog, {
+      width: '500px',
+      data: this.entity,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
+      if (this.entity && result) {
+        if (this.entity.components.some(c => c.component_name === result)) {
+          this._snackBar.open('Component already exists', 'Close', {
+            duration: 2000,
+          });
+        } else {
+          // TODO: Get component data and add it to the entity
+        }
+      }
+    });
+  }
+}
+
+@Component({
+  selector: 'add-component-dialog',
+  templateUrl: 'add-component-dialog.html',
+})
+export class AddComponentDialog {
+  formControl = new FormControl('');
+  // TODO: Get available components
+  options: string[] = ['Position', 'Velocity', 'Sprite'];
+  filteredOptions: Observable<string[]>;
+
+  constructor(
+    public dialogRef: MatDialogRef<AddComponentDialog>,
+    @Inject(MAT_DIALOG_DATA) public entity: Entity,
+  ) {
+    this.filteredOptions = this.formControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || ''))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+  onAddClick(): void {
+    const componentName = this.formControl.value;
+    this.dialogRef.close(componentName);
+  }
 }
