680ec08e179f9bd6c497f359d5ed12251bb32e65
Maxim Gorbach <Maxim.Gorbach@warwick.ac.uk>
2023-03-24 17:06:07 +0000

    changed runner game folder to load assets properly

--- runner/game/scripts/components/Sprite.js
+++ runner/game/scripts/components/Sprite.js
@@ -2,19 +2,23 @@
 import Position from "./Position.js";
 
 export default class Sprite extends engine.Component {
-    static arg_names = ["texAlias", "layer", "z-index"];
-    static arg_types = [engine.Types.String, engine.Types.String, engine.Types.Number];
+    static arg_names = ["texAlias", "layer", "z-index", "rotation", "anchor x", "anchor y"];
+    static arg_types = [engine.Types.String, engine.Types.String, engine.Types.Number, engine.Types.Number, engine.Types.Number, engine.Types.Number];
     dependencies = [Position];
     tex;
     layer;
     zindex;
     lr;
+    rotation;
+    anchor;
     //sprites will be drawn above objects with a lower z index than their own
-    constructor(texAlias, lr, zi = 0) {
+    constructor(texAlias, lr, zi = 0, rotation = 0, anchorX = 0.5, anchorY = 0.5) {
         super();
         this.tex = texAlias;
         this.zindex = zi;
         this.lr = lr;
+        this.rotation = rotation * Math.PI / 2;
+        this.anchor = { x: anchorX, y: anchorY };
     }
     onCreate() {
         const layer = engine.Renderer.getLayer(this.lr, this.owner.scene);
