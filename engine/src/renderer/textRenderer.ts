fac3d9b17111763aa3b1a92a67e91de14526b1b7
Maxim Gorbach <Maxim.Gorbach@warwick.ac.uk>
2023-03-30 15:05:06 +0100

    Text no longer persists between scenes

--- engine/src/renderer/textRenderer.ts
+++ engine/src/renderer/textRenderer.ts
@@ -3,6 +3,7 @@ import { $gl } from "./gl.js";
 import { Texture } from "../types.js";
 import { Renderer, RenderLayer } from "./renderer.js";
 import Text from '../components/Text';
+import { SceneManager } from "../sceneManager.js";
 
 const { mat4, vec3 } = require("gl-matrix");
 const nw = (window as any).nw;
@@ -126,6 +127,7 @@ export class TextRenderer {
       // render each text instance
       for (const text of this.texts) {
         if (!text.visible) continue;
+        if (text.owner.scene != SceneManager.currentScene) continue;
         // first bind the font texture
         this.bindFontTexture(text.font);
         this.renderTextInstance(text);
