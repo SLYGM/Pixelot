7ee1fb77b0567eb7b137be0ad0ebdfb1321f4618
Louis Tarvin <me@louistarvin.uk>
2023-03-30 14:44:57 +0100

    Fixes

--- tests/src/gameloop.test.ts
+++ tests/src/gameloop.test.ts
@@ -1,8 +1,17 @@
-import { expect, test } from "@jest/globals";
-import { Game, Renderer } from "retro-engine";
+import { beforeAll, expect, jest, test } from "@jest/globals";
+import { GLUtils, Game, Renderer, SceneManager, doProjectImports, loadGL } from "retro-engine";
+
+beforeAll(async () => {
+    // Loading webgl in the test environment doesn't work, so we tell jest to mock these functions
+    Renderer.setResolution = jest.fn();
+    // @ts-ignore
+    Game.loadTextures = jest.fn();
+
+    await doProjectImports("test_project", "test");
+    SceneManager.project_dir = "./projects/test_project/";
+});
 
 test("Load game", () => {
-    Renderer.init(true);
     Game.loadGame("./projects/test_project/");
     expect(Game.start_scene).toBe("test_scene");
 });
